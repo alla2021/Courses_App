bd6a5f197bf3ed3e0845fd75d56a33e22669ebee
Alla Honcharova <alla7gon@gmail.com>
2023-11-15 15:56:04 +0200

    fixed styles

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import {Box, Container} from '@mui/material';
 
 export default function Loader() {
     return (
-        <Box sx={{ display: 'flex' }}>
-            <CircularProgress />
-        </Box>
+        <Container maxWidth="xl">
+            <Box sx={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+                <CircularProgress />
+            </Box>
+        </Container>
     );
 }
